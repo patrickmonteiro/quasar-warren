b4b5a8e6497767ffd26295b2dcaeb111cc50609c
Waldener Monteiro <jjrr019@gmail.com>
2021-01-08 02:28:29 -0300

    feature(conta): Criando rota para o componente conta

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'cash', component: () => import('pages/Account.vue') }
     ]
   },
 
