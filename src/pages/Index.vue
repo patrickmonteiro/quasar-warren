69df3bff6dcfcd8c13bbe8077f4b34debc2f01ac
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-01-08 00:19:51 -0300

    feat: create index page

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding>
+    <overview />
+    <line-overview />
+    <div class="row">
+      <div class="col-12 q-pt-xl">
+        <q-btn
+          color="primary"
+          label="Fazer Transação"
+          class="text-bold"
+          rounded
+          unelevated
+        />
+      </div>
+    </div>
+    <list-section />
+    <list-product />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  components: {
+    Overview: () => import('components/Overview'),
+    LineOverview: () => import('components/charts/LineOverview'),
+    ListSection: () => import('components/lists/ListSection'),
+    ListProduct: () => import('components/lists/ListProduct')
+  }
+}
+</script>
