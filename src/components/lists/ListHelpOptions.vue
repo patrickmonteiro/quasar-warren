52017cf71ce717c871aa04e939bc5e394e2fae5c
Waldener Monteiro <jjrr019@gmail.com>
2021-01-08 09:57:12 -0300

    refactor(account): add container in component

--- src/components/lists/ListHelpOptions.vue
+++ src/components/lists/ListHelpOptions.vue
@@ -1,25 +1,27 @@
 <template>
-  <q-list separator class="rounded-borders q-pb-xl">
-    <q-item
-      v-for="(item, index) in items"
-      :key="index"
-      clickable
-      v-ripple
-      class="bg-white rounded-borders q-py-md"
-    >
-      <q-item-section avatar>
-        <q-avatar :color="item.color" text-color="white" :icon="item.icon" />
-      </q-item-section>
-      <q-item-section>
-        <q-item-label class="text-body1"> {{ item.title }}</q-item-label>
-      </q-item-section>
-      <q-item-section side>
-        <div class="text-dark text-body1">
-          <q-icon name="fas fa-chevron-right" color="grey-5" />
-        </div>
-      </q-item-section>
-    </q-item>
-  </q-list>
+  <div class="q-pt-lg q-pb-lg">
+    <q-list separator class="rounded-borders">
+      <q-item
+        v-for="(item, index) in items"
+        :key="index"
+        clickable
+        v-ripple
+        class="bg-white rounded-borders q-py-md"
+      >
+        <q-item-section avatar>
+          <q-avatar :color="item.color" text-color="white" :icon="item.icon" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-body1"> {{ item.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <div class="text-dark text-body1">
+            <q-icon name="fas fa-chevron-right" color="grey-5" />
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 <script>
 export default {
