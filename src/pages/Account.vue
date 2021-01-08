1a8bdc3e0f9f5375f3c8ceb4d85e091116d6f4f7
Waldener Monteiro <jjrr019@gmail.com>
2021-01-08 02:28:53 -0300

    feature(conta): Criando a página conta

---
+++ src/pages/Account.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page padding>
+    <div class="row">
+      <div class="col-md-8 col-sm-12 col-xs-12">
+        <account-overview />
+        <line-account-overview />
+        <div class="row">
+          <div class="col-12 q-pt-xl">
+            <q-btn
+              color="positive"
+              label="Fazer Transação"
+              class="text-bold"
+              rounded
+              unelevated
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12 col-xs-12">
+          <list-account-section> </list-account-section>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  components: {
+    AccountOverview: () => import('components/AccountOverview'),
+    LineAccountOverview: () => import('components/charts/LineAccountOverview'),
+    ListAccountSection: () => import('components/lists/ListAccountSection')
+  }
+}
+</script>
